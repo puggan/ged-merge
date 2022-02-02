9880c61bccd186c5e085d214970e88a4b2db9333
Puggan Sundragon <thorbjorn.sundragon@rolfsbuss.se>
2022-02-02 19:51:51 +0100

    namelsit updated

--- nameList.php
+++ nameList.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 (static function($file) {
     $ged = $file ? new GedMerger($file): GedMerger::instance();
-    foreach($ged->parseRows([0 => ['INDI'], 1 => ['NAME'], 2=> false]) as $child) {
-        echo $child->children['NAME'][0]->value ?? '(unknown)', PHP_EOL;
+    foreach($ged->nameList() as $nameRow) {
+        echo $nameRow, PHP_EOL;
     }
 })($argv[1] ?? '');
